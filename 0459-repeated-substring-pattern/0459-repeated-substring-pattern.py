5c7ad4afc46aa7b66f62fe653c8920f11876fd2d
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-08-21 09:47:08 +0530

    Time: 45 ms (84.13%), Space: 16.4 MB (75.82%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -6,8 +6,4 @@ class Solution:
         # if s1 in s:
         #     return True
         # return False
-        for i in range(1,len(s)//2+1):
-            if len(s)%i==0:
-                if s[:i]*(len(s)//i)==s:
-                    return True
-        return False
+        return True if s in (s*2)[1:-1] else False
