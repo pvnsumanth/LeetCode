32ccd047b06eb1c01bd0e4e43c0869fba17e6096
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-11 07:18:30 +0530

    Time: 0 ms (100%), Space: 6.2 MB (92.67%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string>str;
+        stack<int>count;
+        string ans="";
+        int num=0;
+        for (int i=0;i<s.size();i++){
+           if (s[i]>='0' && s[i]<='9'){ 
+               num=10*num+s[i]-'0';
+           }
+           else if (s[i]=='[') {
+                str.push(ans);
+                ans="";
+                count.push(num);
+                num=0;
+           }
+           else if(s[i]==']'){ 
+               string temp=ans;
+               ans=str.top();
+               str.pop();
+               int times=count.top();
+               count.pop();
+               while (times--){
+                   ans+=temp;
+               }
+           }
+           else { // for char 
+               ans+=s[i];
+           }
+
+        }
+        return ans;
+    }
+};
