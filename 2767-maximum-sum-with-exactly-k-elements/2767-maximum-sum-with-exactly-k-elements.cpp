f93f2e1424eb39697b8d28d128e78d4f88eaa3ab
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-12 12:01:32 +0530

    Time: 38 ms (44.08%), Space: 70.8 MB (11.57%) - LeetHub

--- 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.cpp
+++ 2767-maximum-sum-with-exactly-k-elements/2767-maximum-sum-with-exactly-k-elements.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        return (*max_element(nums.begin(),nums.end()))*k+((k-1)*(k)/2);
+        return *std::max_element(nums.begin(),nums.end())*k+((k-1)*(k)/2);
     }
 };
