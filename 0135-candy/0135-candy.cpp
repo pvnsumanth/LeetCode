f4498d475f12b290d182dc5066511dd5e9ed426d
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-14 07:03:23 +0530

    Time: 17 ms (61.54%), Space: 18.3 MB (15.67%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -9,7 +9,7 @@ public:
             if(ratings[i]>ratings[i+1]) nums[i]=max(nums[i+1]+1,nums[i]);
         }
         int sum=0;
-        for(auto i:nums) sum+=i;
+        for(int i:nums) sum+=i;
         return sum;
     }
 };
