334765c490f03172adb1cb5f3d49424dc6efd11e
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-15 07:13:56 +0530

    Time: 0 ms (100%), Space: 7.6 MB (28.8%) - LeetHub

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -4,14 +4,12 @@ public:
         int max=INT_MIN;
         int min=INT_MAX;
         double sum=0;
-        int count=0;
         for(auto i:salary){
             if(i>max)max=i;
             if(i<min)min=i;
             sum+=i;
-            count+=1;
         }
-        double ans=(sum-max-min)/(count-2);
-        return ans;
+        sum-=min+max;
+        return sum/(salary.size()-2);
     }
 };
