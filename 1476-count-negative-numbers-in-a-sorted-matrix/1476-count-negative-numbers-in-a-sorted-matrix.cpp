1d829b2c651a8a38ff14875a11289be3b1bebb39
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-19 07:20:11 +0530

    Time: 15 ms (35.78%), Space: 10.9 MB (5.81%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -16,22 +16,4 @@ public:
         }
         return count;
     }
-    // int countNegatives(vector<vector<int>>& grid) {
-    //     int count=0;
-    //     for(auto i:grid){
-    //         int left=0;
-    //         int right=i.size()-1;
-    //         while(left<=right){
-    //             int mid=(left+right)/2;
-    //             if(i[mid]<0){
-    //                 count+=right-mid+1;
-    //                 right=mid-1;
-    //             }
-    //             else{
-    //                 left=mid+1;
-    //             }
-    //         }
-    //     }
-    //     return count;
-    // }
 };
