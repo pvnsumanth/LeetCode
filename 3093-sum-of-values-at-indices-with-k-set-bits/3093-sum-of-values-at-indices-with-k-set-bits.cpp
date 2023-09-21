2f626b8985245a00b41723b752d7e22cb9e09cee
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-21 07:52:51 +0530

    Time: 11 ms (44.74%), Space: 21.8 MB (94.23%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.cpp
@@ -4,13 +4,13 @@ public:
 {
       int count = 0;
       while(n) {
-          if(n%2 == 1) count++;
+          count+=n%2;
           n /= 2;
       }
       return count;
 }
     int sumIndicesWithKSetBits(vector<int>& nums, int k) {
-        long long ans = 0;
+        int ans = 0;
         for(int i=0; i<nums.size(); i++) {
             if(countSetBits(i) == k) {
                 ans += nums[i];
