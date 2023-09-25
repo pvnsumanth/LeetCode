1fc3f1b16e5be92be99878fdd21fe45981399501
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-25 07:13:37 +0530

    Time: 2 ms (70.52%), Space: 7 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int>hash(128,0);
+        vector<int>hash(26,0);
         for(auto i:s){
-            hash[i]+=1;
+            hash[i-'a']+=1;
         }
         for(auto i:t){
-            hash[i]-=1;
-            if(hash[i]<0){
+            hash[i-'a']-=1;
+            if(hash[i-'a']<0){
                 return i;
             }
         }
