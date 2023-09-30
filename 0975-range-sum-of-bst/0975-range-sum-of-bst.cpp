7fe80e7024b93de0957ef4b55e23fb25504b5253
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-09-30 08:10:12 +0530

    Time: 82 ms (95.14%), Space: 67.3 MB (6.48%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.cpp
@@ -14,9 +14,9 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         int sum=0;
         queue<TreeNode*>q;
-        if(root) q.push(root);
+        q.push(root);
         while(!q.empty()){
-            TreeNode* node=q.front();
+            auto node=q.front();
             q.pop();
             if(node->val>=low && node->val<=high) sum+=node->val;
             if(node->left) q.push(node->left);
