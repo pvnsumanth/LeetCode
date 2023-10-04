7b0aa1e50958803e366c374e9058b0b53b36e5e5
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-10-04 07:19:46 +0530

    Time: 11 ms (69.24%), Space: 12.8 MB (5.73%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>hash;
+        for(int i=0;i<nums.size();i++) hash[nums[i]]=i;
+        for(int i=0;i<nums.size();i++){
+            if(hash.find(target-nums[i])!=hash.end() &&hash[target-nums[i]]!=i) return {i,hash[target-nums[i]]};
+        }
+        return {};
+    }
+};
