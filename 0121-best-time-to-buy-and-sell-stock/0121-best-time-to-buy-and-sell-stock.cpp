b265e6a5b7ab383fad842a39a130645fff96de7e
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-10-06 07:37:40 +0530

    Time: 105 ms (18.02%), Space: 93.7 MB (21.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,6 @@ public:
         int profit=0;
         int start=0;
         for(int end=1;end<prices.size();end++){
-            cout<<start<<" "<<end<<endl;
             if(prices[start]>prices[end])start=end;
             profit=max(profit,prices[end]-prices[start]);
         }
