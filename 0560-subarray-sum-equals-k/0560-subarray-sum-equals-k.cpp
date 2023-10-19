962c7bb3e7e1cb8a5bee51cccada59e98908fe9d
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-10-19 08:00:00 +0530

    Time: 102 ms (20.09%), Space: 44.2 MB (46.2%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,11 +3,9 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         map<int,int>hash;
         hash[0]=1;
+        for(int i=1;i<nums.size();i++) nums[i]+=nums[i-1];
         int count=0;
-        if(hash.find(nums[0]-k)!=hash.end()) count+=hash[nums[0]-k];
-        hash[nums[0]]++;
-        for(int i=1;i<nums.size();i++){
-            nums[i]+=nums[i-1];
+        for(int i=0;i<nums.size();i++){
             if(hash.find(nums[i]-k)!=hash.end()) count+=hash[nums[i]-k];
             hash[nums[i]]++;
         }
