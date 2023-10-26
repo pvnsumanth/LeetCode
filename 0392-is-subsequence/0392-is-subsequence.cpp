48969f749666404dd563d87bcf64c83ed4925c6f
paruchuri sumanth <131793317+pvnsumanth@users.noreply.github.com>
2023-10-26 07:34:47 +0530

    Time: 7 ms (5.13%), Space: 8.3 MB (5.56%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,0));
+        for(int i=1;i<s.size()+1;i++){
+           for(int j=1;j<t.size()+1;j++){
+               if(s[i-1]==t[j-1]){
+                   dp[i][j]=dp[i-1][j-1]+1;
+               }
+               else{
+                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+               }
+           } 
+           
+        }
+        if(dp[s.size()][t.size()]==s.size()) return true;
+        return false;
+    }
+};
